3ee6ed0e9461d3eabfc654fb447cc9cba05adefb
Daniel Allen <dtallen1@cs.washington.edu>
2018-01-16 23:06:32 -0800

    Update ParseFBMessages.java

--- src/p2/wordsuggestor/ParseFBMessages.java
+++ src/p2/wordsuggestor/ParseFBMessages.java
@@ -16,12 +16,27 @@ public final class ParseFBMessages {
     private ParseFBMessages() {
         /* should not be instantiated */ }
 
+    // INSTRUCTIONS:
+    //
+    // <Your FB Name> may be either:
+    //  1) Your name on Messenger (e.g. "Danny Allen")
+    //  2) Your username on facebook, which can be found by looking at the URL on your profile
+    // It's typically 1), but for whatever reason Facebook sometimes labels them
+    // with 2) (sorry!). You can check which one your messages are labeled with by
+    // opening up one of the message files and taking a look.
+    //
+    // <Your FB Archive> is the directory on your computer where the archive is stored.
+    // (e.g. "/Users/Me/Downloads/MyArchiveName" or "C:\Users\Me\Downloads\MyArchiveName")
+    // You may be able to use a relative path like "./MyArchiveName", but results can
+    // vary from machine to machine.
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             System.out.println("USAGE: ParseFBMessages <Your FB Name> <Your FB Archive>");
             System.exit(1);
         }
 
+        // Note: you can replace these with your FB Name and Archive instead of
+        // using the command line if you'd like.
         String name = args[0];
         String archive = args[1];
 
