7256c6b88e6671e3eeea51743a135987700ea101
Annie Mao <anniem4@cs.washington.edu>
2019-01-21 22:20:01 -0800

    Update ChainingHashTable.java

--- src/datastructures/dictionaries/ChainingHashTable.java
+++ src/datastructures/dictionaries/ChainingHashTable.java
@@ -15,7 +15,9 @@ import cse332.interfaces.misc.Dictionary;
  *    any key) or the number of inputs (i.e., it must grow as necessary).
  * 3. Your HashTable should rehash as appropriate (use load factor as
  *    shown in class!). 
- * 5. HashTable should be able to grow more than 200,000 elements.
+ * 5. HashTable should be able to resize its capacity to prime numbers for more 
+ *    than 200,000 elements. After more than 200,000 elements, it should 
+ *    continue to resize using some other mechanism.
  * 6. We suggest you hard code some prime numbers. You can use this
  *    list: http://primes.utm.edu/lists/small/100000.txt 
  *    NOTE: Do NOT copy the whole list!
