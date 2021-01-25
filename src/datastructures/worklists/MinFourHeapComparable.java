1ee846ed0f04cb479f5b40c9cdd9e28c1344bbee
Nardin Eshak <nardineshak@Nardins-MacBook-Pro.local>
2021-01-24 22:42:48 -0800

    Working Add

--- src/datastructures/worklists/MinFourHeapComparable.java
+++ src/datastructures/worklists/MinFourHeapComparable.java
@@ -3,6 +3,8 @@ package datastructures.worklists;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.PriorityWorkList;
 
+import java.util.NoSuchElementException;
+
 /**
  * See cse332/interfaces/worklists/PriorityWorkList.java
  * for method specifications.
@@ -10,38 +12,76 @@ import cse332.interfaces.worklists.PriorityWorkList;
 public class MinFourHeapComparable<E extends Comparable<E>> extends PriorityWorkList<E> {
     /* Do not change the name of this field; the tests rely on it to work correctly. */
     private E[] data;
+    private int size;
+    private E root;
+    private int capacity = 10;
 
     public MinFourHeapComparable() {
-        throw new NotYetImplementedException();
+        data = (E[])new Comparable[capacity];
+        size = 0;
     }
 
     @Override
     public boolean hasWork() {
-        throw new NotYetImplementedException();
+        return (this.size != 0);
     }
 
+    //nardin
     @Override
     public void add(E work) {
-        throw new NotYetImplementedException();
+        //if the array is full double the size of it
+        if(size == capacity){
+            capacity *= 2;
+            E[] temp = (E[]) new Comparable[capacity];
+            for(int i = 0; i < data.length; i++){
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+        if(this.size == 0){
+            data[0] = work;
+            root = work;
+        }else{
+            int index = percolateUp(size, work);
+            data[index] = work;
+            if(index == 0){
+                root = data[0];
+            }
+        }
+        size++;
+    }
+
+    public int percolateUp(int index, E work){
+        while(index > 0 && (work.compareTo(data[index / 4]) < 0)) {
+            data[index] = data[index / 4];
+            index /= 4;
+        }
+        return index;
     }
 
     @Override
     public E peek() {
-        throw new NotYetImplementedException();
+        if(this.hasWork() == false){
+            throw new NoSuchElementException();
+        }
+        return root;
     }
 
+    //julian
     @Override
-    public E next() {
-        throw new NotYetImplementedException();
+    public E next() { throw new NotYetImplementedException();
     }
+
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
     }
 
     @Override
     public void clear() {
-        throw new NotYetImplementedException();
+        data = (E[]) new Comparable[10];
+        root = null;
+        size = 0;
     }
 }
