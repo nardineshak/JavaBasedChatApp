c30eff962c012b198ba64079c77c2e081dc58317
keys <keys@reef.local>
2021-02-01 03:34:23 -0800

    AVLTree passing all tests

--- src/datastructures/dictionaries/AVLTree.java
+++ src/datastructures/dictionaries/AVLTree.java
@@ -2,32 +2,87 @@ package datastructures.dictionaries;
 
 import cse332.datastructures.trees.BinarySearchTree;
 
-/**
- * TODO: Replace this comment with your own as appropriate.
- *
- * AVLTree must be a subclass of BinarySearchTree<E> and must use
- * inheritance and calls to superclass methods to avoid unnecessary
- * duplication or copying of functionality.
- *
- * 1. Create a subclass of BSTNode, perhaps named AVLNode.
- * 2. Override the insert method such that it creates AVLNode instances
- *    instead of BSTNode instances.
- * 3. Do NOT "replace" the children array in BSTNode with a new
- *    children array or left and right fields in AVLNode.  This will 
- *    instead mask the super-class fields (i.e., the resulting node 
- *    would actually have multiple copies of the node fields, with 
- *    code accessing one pair or the other depending on the type of 
- *    the references used to access the instance).  Such masking will 
- *    lead to highly perplexing and erroneous behavior. Instead, 
- *    continue using the existing BSTNode children array.
- * 4. If this class has redundant methods, your score will be heavily
- *    penalized.
- * 5. Cast children array to AVLNode whenever necessary in your
- *    AVLTree. This will result a lot of casts, so we recommend you make
- *    private methods that encapsulate those casts.
- * 6. Do NOT override the toString method. It is used for grading.
- */
-
 public class AVLTree<K extends Comparable<? super K>, V> extends BinarySearchTree<K, V>  {
-    // TODO: Implement me!
+    private static final int ALLOWED_IMBALANCE = 1;
+
+    @Override
+    public V insert(K key, V value) {
+        V oldValue = super.find(key);
+        this.root = insert(key, value, toAVL(this.root));
+        return oldValue;
+    }
+
+    private AVLNode insert(K key, V value, AVLNode t) {
+        if (t == null) {
+            this.size++;
+            return new AVLNode(key, value);
+        }
+        // compare key of node to insert and subtree root
+        int compareResult = Integer.signum(key.compareTo(t.key));
+
+        if (compareResult == 0) {
+            t.value = value;
+            return t;
+        }
+
+        if (compareResult < 0) {
+            t.children[0] = insert(key, value, toAVL(t.children[0]));
+        } else {
+            t.children[1] = insert(key, value, toAVL(t.children[1]));
+        }
+        return balance(t);
+    }
+
+    private AVLNode balance (AVLNode t) {
+        if (t == null) {
+            return t;
+        }
+        int diff = height(toAVL(t.children[0])) - height(toAVL(t.children[1]));
+        int imbalance = Math.abs(diff);
+        if (imbalance > ALLOWED_IMBALANCE) {
+            int child = (diff > 0) ? 0 : 1;
+            if (height(toAVL(t.children[child].children[child])) < height(toAVL(t.children[child].children[1-child]))) {
+                t.children[child] = rotateWithChild(1-child, toAVL(t.children[child]));
+            }
+            t = rotateWithChild(child, t);
+        }
+        t.height = Math.max(height(toAVL(t.children[0])), height(toAVL(t.children[1]))) + 1;
+        return t;
+    }
+
+    private AVLNode rotateWithChild(int child, AVLNode k2) {
+        AVLNode k1 = toAVL(k2.children[child]);
+        k2.children[child] = k1.children[1-child];
+        k1.children[1-child] = k2;
+        k2.height = Math.max(height(toAVL(k2.children[child])), height(toAVL(k2.children[1-child]))) + 1;
+        k1.height = Math.max(height(toAVL(k1.children[child])), k2.height) + 1;
+        return k1;
+    }
+
+    protected class AVLNode extends BSTNode {
+        public int height;
+
+        public AVLNode(K key, V value) {
+            super(key, value);
+            this.height = 0;
+        }
+    }
+
+    /**
+     * Return the height of node t, or -1, if null.
+     */
+    private int height(AVLNode t) {
+        return t == null ? -1 : t.height;
+    }
+
+    /**
+     * toAVL casts BSTNode node to AVLNode
+     */
+    @SuppressWarnings("unchecked")
+    private AVLNode toAVL(BinarySearchTree.BSTNode node) {
+        if (node instanceof AVLTree.AVLNode) {
+            return (AVLNode) node;
+        }
+        return null;
+    }
 }
